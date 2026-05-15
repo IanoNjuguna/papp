e18f198c79a84f3501f6e5f7b566e44388f9b7e0
IanoNjuguna <inc3099@gmail.com>
2026-05-15 21:20:54 +0300

    remove React bolierplate

--- papp-src/dapp-vue/src/components/Header.vue
+++ papp-src/dapp-vue/src/components/Header.vue
@@ -9,7 +9,7 @@ import Connect from './Connect.vue'
         <a href="https://polkadot.com" target="_blank" class="flex items-center">
           <span class="icon-[token-branded--polkadot] text-2xl" />
           <span class="text-xl font-bold font-mono text-black tracking-wide ml-1">
-            Dapp
+            papp
           </span>
         </a>
       </div>
